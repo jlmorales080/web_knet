748a0f0b931e159161086d2be7e8637f9300d138
Juan Luis <jlmorales080@gmail.com>
2026-01-23 00:10:24 -0500

    Fix offers layout title vertical center, structure, and backup db (KnetCMS_20260123.bak)

--- Controllers/HomePageController.cs
+++ Controllers/HomePageController.cs
@@ -3,23 +3,33 @@ using Microsoft.AspNetCore.Mvc.ViewEngines;
 using Umbraco.Cms.Core.Web;
 using Umbraco.Cms.Web.Common.Controllers;
 using Umbraco.Cms.Web.Common.PublishedModels;
+using Umbraco.Cms.Core.Models;
 
 namespace KnetCMS.Controllers
 {
     public class HomePageController : RenderController
     {
+        private readonly ILogger<RenderController> _logger;
+
         public HomePageController(
             ILogger<RenderController> logger,
             ICompositeViewEngine compositeViewEngine,
             IUmbracoContextAccessor umbracoContextAccessor)
             : base(logger, compositeViewEngine, umbracoContextAccessor)
         {
+            _logger = logger;
         }
 
         public override IActionResult Index()
         {
-            // Explicit view path to solve discovery issues in this environment
+            _logger.LogInformation("HomePageController.Index() Hit!");
             return View("~/Views/HomePage.cshtml", CurrentPage);
+        }
+
+        public IActionResult HomePage(ContentModel model)
+        {
+            _logger.LogInformation("HomePageController.HomePage() Hit!");
+            return View("~/Views/HomePage.cshtml", model.Content);
         }
     }
 }
